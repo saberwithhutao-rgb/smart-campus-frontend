e0d707566255bafb85e7e97d290d01f6d1477761
saber <saberwithhutao@gamil.com>
2026-02-05 19:23:26 +0800

    修改

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { api } from '../api/index'
 
